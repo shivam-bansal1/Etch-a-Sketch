ea0fe6ebb86046b6ebee07e68e179416ffbf3c7e
Shivam Bansal <shivambnsal@gmail.com>
2024-07-08 18:24:29 +0530

    Customize grid size
    User can alter the grid size

--- js/script.js
+++ js/script.js
@@ -1,12 +1,30 @@
 const container = document.querySelector('.container');
 
-for(let row=0; row<256; row++) {   
-        const squareDiv = document.createElement('square-div');
-        squareDiv.classList.add('square-div');
-        // squareDiv.textContent = row + 1;
-        const widthHeight = 960/16;
-        console.log(widthHeight);
-        squareDiv.style.width = `${widthHeight}px`;
-        squareDiv.style.height = `${widthHeight}px`;
-        container.appendChild(squareDiv);
+function changeGridSize(gridSize) {
+	container.replaceChildren();
+	for(let index=0; index < gridSize*gridSize; index++) {   
+		const squareDiv = document.createElement('square-div');
+		squareDiv.classList.add('square-div');
+
+		const widthHeight = Math.round(720/gridSize);
+		squareDiv.style.width = `${widthHeight}px`;
+		squareDiv.style.height = `${widthHeight}px`;
+
+		container.appendChild(squareDiv);
+	}
 }
+
+let gridSizeChoice = 16;
+changeGridSize(gridSizeChoice);
+
+const gridChangeButton = document.querySelector('.grid-button');
+
+gridChangeButton.addEventListener('click', () => {
+	gridSizeChoice = prompt("Enter the grid size(Between 10 and 100): ");
+	console.log(gridSizeChoice);
+	
+	if(gridSizeChoice === null) 
+		changeGridSize(16);
+	else
+		changeGridSize(gridSizeChoice);
+})
