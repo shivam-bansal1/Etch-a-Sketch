475a71456cd73a75e1b6491d650d19be8c6ad308
Shivam Bansal <shivambnsal@gmail.com>
2024-07-09 17:48:37 +0530

    Fix grid padding bug
    Bug: Sapce was left side of grid container on different values of grid.
    Solution: Calculate size of divs in js using the size of container
              instead of hard coding the size.

--- js/script.js
+++ js/script.js
@@ -2,18 +2,23 @@ const container = document.querySelector('.container');
 
 function changeGridSize(gridSize) {
 	container.replaceChildren();
+
+	// To get style of container div
+	const containerComputedStyle = window.getComputedStyle(container);
+	const containerWidth = parseFloat(containerComputedStyle.width);
+    const containerHeight = parseFloat(containerComputedStyle.height);
+
 	for(let index=0; index < gridSize*gridSize; index++) {   
 		const squareDiv = document.createElement('square-div');
 		squareDiv.classList.add('square-div');
-
 		const randomColor = generateRandomColor()
-		console.log(randomColor);
 
-		const widthHeight = Math.round(720/gridSize);
-		squareDiv.style.width = `${widthHeight}px`;
-		squareDiv.style.height = `${widthHeight}px`;
+		const itemWidth = containerWidth / gridSize;
+		const itemHeight = containerHeight / gridSize;
+
+		squareDiv.style.width = `${itemWidth}px`;
+		squareDiv.style.height = `${itemHeight}px`;
 		squareDiv.style.backgroundColor = randomColor;
-
 		container.appendChild(squareDiv);
 
 		squareDiv.addEventListener('mouseover',() => squareDiv.style.opacity = 1);
@@ -21,14 +26,14 @@ function changeGridSize(gridSize) {
 }
 
 // Grid Size Code
-let gridSizeChoice = 16;
+let gridSizeChoice = 11;
 changeGridSize(gridSizeChoice);
 
 const gridChangeButton = document.querySelector('.grid-button');
 
 gridChangeButton.addEventListener('click', () => {
 	gridSizeChoice = prompt("Enter the grid size(Between 10 and 100): ");
-	console.log(gridSizeChoice);
+	console.log(`gridSizeChoice: ${gridSizeChoice}`);
 	
 	if(gridSizeChoice === null) 
 		changeGridSize(16);
